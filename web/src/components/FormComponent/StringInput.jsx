7ebd39bbc0f49f5b6a8a3c687df2579fa9b253ba
JamieHenry <jhenry@skillstorm.com>
2022-06-21 14:07:14 -0400

    Set Min String size for Airport Inputs

--- web/src/components/FormComponent/StringInput.jsx
+++ web/src/components/FormComponent/StringInput.jsx
@@ -2,7 +2,7 @@ export const StringInput = ({ id, children }) => {
     return (
         <>
             <label htmlFor={id}>{children}</label>
-            <input type='text' id={id} name='string-input' maxLength={3} style={{width: '30px'}} />
+            <input type='text' id={id} name='string-input' minLength ={3} maxLength={3} style={{width: '30px'}} />
         </>
     );
 }
