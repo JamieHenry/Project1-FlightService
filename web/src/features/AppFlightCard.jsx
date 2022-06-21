e61ae0c9c21eb1e40a79d1c8866b5e80a3bf5283
JamieHenry <jhenry@skillstorm.com>
2022-06-21 17:59:30 -0400

    Add delete confirmation modal

--- web/src/features/AppFlightCard.jsx
+++ web/src/features/AppFlightCard.jsx
@@ -1,8 +1,14 @@
 import axios from 'axios';
-import { OuterContainer, InnerContainer, Item, TotalTime, Date, Airport, PassengerInfo, ButtonBox, Button, Time, FlightNo } from '../components/FlightCard';
 import arrow from '../assets/arrow.png';
+import { useState } from 'react';
+import { ConfirmationModal } from '../components/ConfirmationModal';
+import { OuterContainer, InnerContainer, Item, TotalTime, Date, Airport, PassengerInfo, ButtonBox, Button, Time, FlightNo } from '../components/FlightCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const AppFlightCard = ({ flight, margin, updateFlights }) => {
+
+    const [displayConfirmation, setDisplayConfirmation] = useState(false);
+    const [deleteMessage, setDeleteMessage] = useState('');
 
     const calcHourDiff = () => {
         const [startMonth, startDay, startYear] = flight.departureDate.split('/');
@@ -35,17 +41,29 @@ export const AppFlightCard = ({ flight, margin, updateFlights }) => {
         console.log(flight.flightNumber + " edit flight");
     }
 
-    const deleteFlight = () => {
+    const showDeleteModal = () => {
+        setDeleteMessage(`Are you sure you want to delete Flight: ${flight.flightNumber}?`);
+        setDisplayConfirmation(true);
+    }
+
+    const hideConfirmation = () => {
+        setDisplayConfirmation(false);
+    }
+
+    const submitDelete = () => {
         axios.delete(`http://localhost:8080/flights/${flight.flightNumber}`)
             .then(res => {
                 console.log(res.data);
                 updateFlights();
             })
             .catch(err => console.log(err));
+        setDisplayConfirmation(false);
     }
 
     return (
-        <OuterContainer margin={margin}>
+        <>
+            <ConfirmationModal showModal={displayConfirmation} confirmModal={submitDelete} hideModal={hideConfirmation} message={deleteMessage} />
+            <OuterContainer margin={margin}>
             <InnerContainer>
                 <FlightNo>{flight.flightNumber}</FlightNo>
                 <Item />
@@ -62,8 +80,9 @@ export const AppFlightCard = ({ flight, margin, updateFlights }) => {
             </InnerContainer>
             <ButtonBox>
                 <Button onClick={editFlight} bc='green' bcHover='darkgreen' name='fa fa-edit' />
-                <Button onClick={deleteFlight} bc='red' bcHover='darkred' name='fa fa-trash' />
+                <Button onClick={showDeleteModal} bc='red' bcHover='darkred' name='fa fa-trash' />
             </ButtonBox>
         </OuterContainer>
+        </> 
     );
 }
