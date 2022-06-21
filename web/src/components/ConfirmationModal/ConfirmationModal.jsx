cdfac66b7c195dbd8d74ef31f8e0d4b880659613
JamieHenry <jhenry@skillstorm.com>
2022-06-21 18:12:01 -0400

    Add delete confirmation modal

---
+++ web/src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -0,0 +1,20 @@
+import { Modal, Button } from 'react-bootstrap';
+
+export const ConfirmationModal = ({ showModal, hideModal, confirmModal, message }) => {
+    return (
+        <Modal show={showModal} onHide={hideModal}>
+            <Modal.Header closeButton>
+            <Modal.Title>Delete Confirmation</Modal.Title>
+            </Modal.Header>
+            <Modal.Body><div className="alert alert-danger">{message}</div></Modal.Body>
+            <Modal.Footer>
+            <Button variant="default" onClick={hideModal}>
+                Cancel
+            </Button>
+            <Button variant="danger" onClick={confirmModal}>
+                Delete
+            </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
